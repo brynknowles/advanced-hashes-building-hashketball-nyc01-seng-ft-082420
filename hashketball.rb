d9c7519f138190ae027d10ff59b7bef05d6765fd
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-28 01:30:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,20 +128,6 @@ def game_hash
   }
 end
 
-# def game_hash
-#   {
-#     home: {
-#       team_name: "Brooklyn Nets", 
-#       colors: ["Black", "White"],
-#       players: [{}]
-#     },
-#     away: {
-#       team_name: "Charlotte Hornets", 
-#       colors: ["Turquoise", "Purple"],
-#       players: [{}]
-#     }
-#   }
-# end
 
 # def game_hash
 #   {
