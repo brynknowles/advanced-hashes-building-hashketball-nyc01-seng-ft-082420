82a06afc8e93ed9cf08e0582a1994ff4e5030b0f
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-28 01:06:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -69,7 +69,7 @@ def game_hash
       colors: ["Turquoise", "Purple"],
       players: [
         {
-          player_name: "Jeff Adrien", 
+          :player_name => "Jeff Adrien", 
           number: 4, 
           shoe: 18, 
           points: 10, 
